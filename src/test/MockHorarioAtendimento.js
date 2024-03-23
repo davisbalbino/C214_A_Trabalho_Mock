8e126ab16ed2d13b62b99a7f0b137c9f2fbf9222
davisbalbino <davidossantosbalbinomarcelino@gmail.com>
2024-03-23 17:48:12 -0300

    more tests e alterations on mock

--- src/test/MockHorarioAtendimento.js
+++ src/test/MockHorarioAtendimento.js
@@ -24,5 +24,22 @@ class MockHorarioAtendimento {
         list[30] = professores.VITORIA
         return list.includes(id);
     }
+
+    determinarPredio(sala) {
+        const salaNum = parseInt(sala);
+        if (salaNum >= 1 && salaNum <= 5) {
+            return "1";
+        } else if (salaNum >= 6 && salaNum <= 10) {
+            return "2";
+        } else if (salaNum >= 11 && salaNum <= 15) {
+            return "3";
+        } else if (salaNum >= 16 && salaNum <= 20) {
+            return "4";
+        } else if (salaNum >= 21 && salaNum <= 25) {
+            return "5";
+        } else {
+            return "6";
+        }
+    }
     
 } module.exports = MockHorarioAtendimento
